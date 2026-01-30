61b649c9529577f5c2cb6b482915b30af7078474
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-01-29 21:55:43 -0600

    Implement cron job to log a message every 5 seconds

--- cron-ticker/app.js
+++ cron-ticker/app.js
@@ -1,1 +1,12 @@
-console.log('hola mundo')
+const cron = require('node-cron');
+
+let times = 0
+
+// corre cada segundo lo que esta dentro
+cron.schedule('1-59/5 * * * * *', () => {
+    times++
+  console.log('tick cada 5 segundos ejecuta times: ', times);
+});
+
+
+console.log('Inicio')
